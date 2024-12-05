ae944b68aed46fed7fa2765bf487a6190ddba3a7
miampf <miampf@proton.me>
2024-12-05 13:40:14 +0100

    use pname instead of name
    fixup

--- actual-desktop.nix
+++ actual-desktop.nix
@@ -1,16 +1,16 @@
 {appimageTools, fetchurl}:
 
 let
-  name = "actual-budget";
+  pname = "actual-budget";
   version = "24.10.1";
   src = fetchurl {
     url = "https://github.com/actualbudget/actual/releases/download/v${version}/Actual-linux.AppImage";
     sha256 = "sha256-Xqb2wECLy3um8yR7MHnVvEdjRko1waHTpkzi18CgarE=";
   };
-  appimageContents = appimageTools.extractType2 {inherit name src;};
+  appimageContents = appimageTools.extractType2 {inherit pname version src;};
   in
 appimageTools.wrapType2 {
-  inherit name version src;
+  inherit pname version src;
 
   extraInstallCommands = ''
     install -m 444 -D ${appimageContents}/desktop-electron.desktop $out/share/applications/actual.desktop
